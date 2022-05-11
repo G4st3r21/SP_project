4f0a2f9d5e934db80543ad4d4b684ecf441f125c
BratyshevTD <bratyshev2013@mail.ru>
2022-05-11 21:40:02 +0300

    Контрольные события в сфере "Развитие образования"

--- PyScripts/Parsers/Industries/Education/CE_2020.py
+++ PyScripts/Parsers/Industries/Education/CE_2020.py
@@ -1,5 +1,6 @@
-from PyScripts.TableClasses.PublicClasses.SPTable import SPTable
-from PyScripts.TableClasses.PublicClasses.SPTableArbitrary import SPTableArbitrary
+from PyScripts.TableClasses.SPTables.SPTable import SPTable
+from PyScripts.TableClasses.SPTables.SPTableArbitrary import SPTableArbitrary
+
 from PyScripts.base.base_functions import parser_init
 
 def commit_all():
@@ -10,38 +11,45 @@ def commit_all():
 def table_parsing():
     for row in rows:
         if not ('Подпрограмма' in row[0].value or 'Основное мероприятие' in row[0].value):
-            control_event_id = row[0].value.split()[2]
-            control_event_id = control_event_id if control_event_id[-1] != '.' else control_event_id[:-1]
-            code_main_event = '.'.join(control_event_id.split('.')[:2])
+            code = row[0].value.split()[2]
+            code = code if code[-1] != '.' else code[:-1]
+            code_main_event = '.'.join(code.split('.')[:2])
             control_event = row[1].value
-            ControlEvent2020.add(control_event_id, code_main_event, control_event)
+            ControlEvent2020.add(code, code_main_event, control_event)
 
             response_obj = row[2].value
-            response_obj_id = ResponseObj.add(response_obj)
+            id_response = ResponseObj.add(response_obj)
 
-
-
-            planed_date = row[3].value
+            id_pf = 0
+            id_done = 0
+            id_viol = 0
+            date_ce = row[3].value
 
             if row[4].value:
-                event_done = True
-                has_violation = False
-                id_date = DateControlEvent2020.add(row[4].value)
+                # event_done = True
+                # has_violation = False
+                id_pf = 1
+                id_done = 1
+                id_viol = 0
+                date_ce = row[4].value
 
             elif row[5].value:
-                event_done = True
-                has_violation = True
-                id_date = DateControlEvent2020.add(row[5].value)
+                # event_done = True
+                # has_violation = True
+                id_pf = 1
+                id_done = 1
+                id_viol = 1
+                date_ce = row[5].value
 
             elif row[6].value == '-':
-                event_done = False
-                has_violation = False
-                id_date = DateControlEvent2020.add(None)
+                # event_done = False
+                # has_violation = False
+                id_pf = 1
+                id_done = 0
+                id_viol = 0
+                date_ce = None
 
-            if id_date:
-                DateControlEvent2020.add(id_date)
-
-            DateControlEvent2020.add(planed_date, id_date)
+            DateControlEvent2020.add(response_obj, id_response, id_pf, id_done, id_viol, date_ce)
 
             comment = row[7].value
             CommentsCE2020.add(comment)
@@ -49,10 +57,10 @@ def table_parsing():
 
 cols, rows, cur, conn = parser_init('11.1. Контрольные события.xlsx', sheet_number=1, first_str_number=12)
 
-ResponseObj = SPTable('response_obj', 'response_obj', cur, conn)
-ControlEvent2020 = SPTableArbitrary('control_event2020', 'control_event', cur, conn, schema='Education')
-CommentsCE2020 = SPTable('comments_ce2020', 'comment', cur, conn, schema='Education')
-DateControlEvent2020 = SPTableArbitrary('date_control_event2020', 'date', cur, conn, schema='Education')
+ResponseObj = SPTable('response_obj', cur, conn)
+ControlEvent2020 = SPTableArbitrary('control_event2020', cur, conn, schema='Education')
+CommentsCE2020 = SPTable('comments_ce2020',  cur, conn, schema='Education')
+DateControlEvent2020 = SPTableArbitrary('date_control_event2020', cur, conn, schema='Education')
 table_parsing()
 
 
---
+++ PyScripts/TableClasses/SPTables/SPTableArbitrary.py
@@ -0,0 +1,68 @@
+import sys
+
+from PyScripts.TableClasses.SPTables.SPTable import SPTable
+
+
+class SPTableArbitrary(SPTable):
+    def __init__(self, table_name, cur, conn, schema='public'):
+        super().__init__(table_name, cur, conn, schema)
+
+    def get_by_column(self, column_name, title):
+        title = f"'{title}'" if not self.columns[0][1] != 'integer' else f'{title}'
+        self.cur.execute(f"SELECT * FROM {self.schema}.{self.table_name} * WHERE {column_name} = {title}")
+        obj = self.cur.fetchall()
+
+        return obj[0] if obj else False
+
+    def get_by_tuple(self, *args, condition_type='AND'):
+        args = list(args)
+        if len(args) == len(self.columns):
+            get_args = [f"{self.columns[args.index(arg)][0]} = '{arg}'" if type(
+                arg) is str else f"{self.columns[args.index(arg)][0]} = {arg}" for arg in args]
+            self.cur.execute(f"SELECT * FROM {self.schema}.{self.table_name} WHERE {f' {condition_type} '.join(get_args[1:])}")
+        elif len(args) + 1 == len(self.columns):
+            get_args = [f"{self.columns[args.index(arg)+1][0]} = '{arg}'" if type(
+                arg) is str else f"{self.columns[args.index(arg)+1][0]} = {arg}" for arg in args]
+            self.cur.execute(f"SELECT * FROM {self.schema}.{self.table_name} WHERE {f' {condition_type} '.join(get_args)}")
+        obj = self.cur.fetchall()
+
+        return obj[0][0] if obj else False
+
+    def add(self, *args, condition_type='AND'):
+        args = list(args)
+        obj_id = self.get_by_tuple(*args, condition_type)
+        if obj_id:
+            return obj_id
+
+        request_args = ", ".join([f"'{arg}'" if type(arg) is str else str(arg) for arg in args])
+        if len(args) == len(self.columns):
+            self.cur.execute(f"INSERT INTO {self.schema}.{self.table_name} VALUES ({request_args})")
+        elif len(args) + 1 == len(self.columns):
+            if 'integer' in self.columns[0][1]:
+                request_columns = ', '.join([column[0] for column in self.columns[1:]])
+                if self.hasSerialID:
+                    if self.isEmpty:
+                        self.cur.execute(f"INSERT INTO {self.schema}.{self.table_name} VALUES (1, {request_args})")
+                    else:
+                        self.cur.execute(
+                            f"INSERT INTO {self.schema}.{self.table_name}({request_columns}) VALUES ({request_args})")
+
+                    self.isEmpty = False
+                    return self.get_by_tuple(args)
+                else:
+                    self.cur.execute(f"SELECT * FROM {self.schema}.{self.table_name} ORDER BY {self.columns[0][0]}")
+                    obj_id = self.cur.fetchall()
+                    if not obj_id:
+                        obj_id = 1
+                    else:
+                        obj_id = obj_id[-1][0] + 1
+                    self.cur.execute(f"INSERT INTO {self.schema}.{self.table_name} VALUES ({obj_id}, {request_args})")
+
+                    self.isEmpty = False
+                    return obj_id
+        elif len(args) > len(self.columns):
+            sys.exit(f'Слишком много аргументов для метода "add" таблицы {self.table_name}\n'
+                     f'(Ожидалось {len(self.columns)} или {len(self.columns) - 1}, получено {len(args)})')
+        else:
+            sys.exit(f'Слишком мало аргументов для метода "add" таблицы {self.table_name}\n'
+                     f'(Ожидалось {len(self.columns)} или {len(self.columns) - 1}, получено {len(args)})')
