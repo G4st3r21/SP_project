e9edfc5210b0ee8fac13683c2b4c8135136cc711
JAMESLINKCODES <jamesyanlink@gmail.com>
2023-05-28 09:28:30 +0300

    fill fed_indicators2019 table

--- PyScripts/Parsers/RegionalProjects/RPAimsIndicatorsTables.py
+++ PyScripts/Parsers/RegionalProjects/RPAimsIndicatorsTables.py
@@ -1,3 +1,5 @@
+import datetime
+
 from base_functions_RP import get_file_names, get_code, format_title
 
 from openpyxl import load_workbook
@@ -33,22 +35,25 @@ def fill_fed_indicators2019(sheet, code):
     args_dict = {'code': code}
     id_fed_project = RegProject.get_by_custom_filter_expression(args_dict)[0][1]
     title_indicators = []
-    first_aim_and_indicators = False
+    skip_aim = False
+    fed_indicators = False
     for row in sheet.iter_rows():
         if row[0].value is not None:
-            if "2. Цель и показатели регионального проекта" in format_title(row[0].value):
-                first_aim_and_indicators = True
+            row_data = format_title(row[0].value)
+            if "2. Цель и показатели регионального проекта" in row_data:
+                skip_aim = True
                 continue
-            if first_aim_and_indicators:
-                first_aim_and_indicators = False
+            if skip_aim:
+                skip_aim = False
+                fed_indicators = True
                 continue
-            if "3. Результаты регионального проекта" in format_title(row[0].value):
+            if "3. Результаты регионального проекта" in row_data:
                 break
-            if "№ п/п" not in format_title(row[0].value):
-                # todo: define what is fed indicator and what isn't
-                title_indicators.append(format_title(row[0].value))
-    if title_indicators[0]:
-        print(id_fed_project, title_indicators)
+            if fed_indicators and row_data[0].isalpha() \
+                    and "Отсутствует показатель федерального проекта" not in row_data:
+                title_indicators.append(row_data)
+    if title_indicators:
+        # print(code, title_indicators)
         FedIndicators2019.add(id_fed_project, title_indicators[0])  # serial
         FedIndicators2019.commit()
 
@@ -56,6 +61,15 @@ def fill_fed_indicators2019(sheet, code):
 def fill_reg_aims_indicators_names2019():
     """Fills reg_aims_indicators_names2019 table"""
     RegAimsIndicatorsNames2019 = SPTableArbitrary('reg_aims_indicators_names2019', cur, conn)
+    # id serial
+    id_reg_aim = 0
+    fed_indicator_id = 0
+    type_id = 0
+    title_indicator = ""
+    units_of_measurement = ""
+    dynamics_id = 0
+    base_value = 0.0
+    base_date = datetime.datetime.date(0)
 
 
 def fill_types_of_indicators():
@@ -64,35 +78,54 @@ def fill_types_of_indicators():
     # todo: find out if there's another types of indicators
     TypesOfIndicators.add("Дополнительный показатель")
     TypesOfIndicators.commit()
+
+
 # fill_types_of_indicators()
 
 
 def fill_dynamics():
     """Fills dynamics table"""
+    # id serial
+    dynamics = ""
 
 
 def fill_indicators_comments2019():
     """Fills indicators_comments2019 table for each individual schema"""
+    id_reg_indicator = 0
+    comment = ""
 
 
 def fill_indicators_additional_comments2019():
     """Fills indicators_additional_comments2019 table for each individual schema"""
+    id_reg_indicator = 0
+    additional_comment = ""
 
 
 def fill_reg_aims_indicators2019():
     """Fills reg_aims_indicators2019 table for each individual schema"""
+    # serial id
+    indicator_id = 0
+    is_deviation = 0
+    plan_fact = False
+    previous_this_year = False
+    quarter_number = 0
+    indicator = 0.0
 
 
 def fills_reg_indicators_passport_values2019():
     """Fills reg_indicators_passport_values2019 for each individual schema"""
+    # serial id
+    reg_indicator_id = 0
+    year_id = 0
+    value = 0.0
 
 
-path = "D:/КСП/Tables/5_Regionalnye_proekty/5_Региональные проекты/2019 паспорта по состоянию на 23.12.2019"
+path = "D:/Библиотеки/КСП/Tables/5_Regionalnye_proekty/5_Региональные проекты/2019 паспорта по состоянию на 23.12.2019"
 files = get_file_names(path)
 
 for f in files:
     code = get_code(f)
-    if code == 'A2':
+    if code:
         full_path = path + "/" + f
         wb = load_workbook(full_path)
 
--- PyScripts/TableClasses/SPTables/SPTableArbitrary.py
+++ PyScripts/TableClasses/SPTables/SPTableArbitrary.py
@@ -106,7 +106,7 @@ class SPTableArbitrary(SPTable):
         # if obj_id:  # get by tuple didn't work for reg_project table properly. temporary hiding this
         #     return obj_id
 
-        request_args = self._format_arguments(args, has_id=True)
+        request_args = self._format_arguments(args, has_id=False)  # todo: change depending on serial
         request_args = ", ".join(request_args)
         if len(args) == len(self.columns):
             self.cur.execute(f"INSERT INTO {self.schema}.{self.table_name} VALUES ({request_args})")
